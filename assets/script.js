b29b2d5823d093043a557bf60679c3df0591e305
Fabien SHAN <shan.fabien@gmail.com>
2018-01-29 01:33:04 +0900

    Translation + Form

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,3 @@
-// source: https://blog.reecreate.com/sending-contact-form-emails-from-your-github-pages-site-using-ajax-formspree-c7b17e3f5b0b
-$('form#contact_form').validate({
-    messages: { },
-    submitHandler: function(form) {
-      $.ajax({
-        url: "https://formspree.io/shan.fabien@gmail.com",
-        method: "POST",
-        data: $(form).serialize(),
-        dataType: "json",
-        success: function(data) {
-            $("form#contact_form :input").prop("disabled", true);
-            $('#thanks').show();
-        }
-      });
-      return false;
-    }
-  });
+function submitForm() {
+  document.getElementById("myForm").submit();
+}
